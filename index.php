ae9efae0c959339009f370c82b0038187217aebf
lv-gen <e.v.lvov@mail.ru>
2014-08-09 22:43:03 +0400

    One Step

---
+++ index.php
@@ -0,0 +1,109 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="/favicon.ico">
+
+    <title>Theme Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/village-piople/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="/village-piople/css/bootstrap-theme.min.css" rel="stylesheet">
+
+   <link href='http://fonts.googleapis.com/css?family=Cuprum:400,400italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+   
+    <!-- Custom styles for this template -->
+    <link href="/village-piople/css/theme.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body role="document">
+  
+  <!-- Navbar -->
+  <div class="container main-borders margin-top-50">
+  <div class="row">
+  <nav class="navbar piople-navbar" role="navigation">
+  <div class="container">
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav piople-nav">
+        <li class="active"><a href="#">Ссылка</a></li>
+        <li><a href="#">Ссылка</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Действие</a></li>
+            <li><a href="#">Другое действие</a></li>
+            <li><a href="#">Что-то еще</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Отдельная ссылка</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Еще одна отдельная ссылка</a></li>
+          </ul>
+        </li>
+      </ul>
+     
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+  </div>
+ </div>
+</nav>
+<!-- Navbar.END -->
+
+<!-- Главная секция -->
+<div id="main-content" class="container he-100proc main-borders">
+  <div class="row he-100proc">
+    <!-- Left Menu -->
+    <div class="col-md-3 he-100proc">
+      <div class="row he-100proc">
+        <ul class="nav nav-pills nav-stacked nav-pills-piople he-100proc">
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-red"></span><span>Главная</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-blue"></span><span>Профиль</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-green"></span><span>Сообщение</span></a></li>
+        <li class="active"><a href="#"><span class="glyphicon glyphicon-lock item-fiolet"></span><span>Профиль</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-ocean"></span><span>Сообщение</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-sun"></span><span>Профиль</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-worm"></span><span>Сообщение</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-forest"></span><span>Профиль</span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-lock item-stone"></span><span>Сообщение</span></a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- Left Menu.END -->
+
+    <!-- Content Block -->
+    <div class="col-md-9">
+      <div class="row">
+        <div id="dashboard-top" class="col-md-12"> 
+          Dashboard
+        </div>  
+      </div>
+    </div>
+    <!-- Content Block.END -->
+
+  </div> <!-- row -->
+</div> <!-- Conteiner --> 
+<!-- Главная секция.END -->   
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/village-piople/js/jquery-1.7.1.js"></script>
+    <script src="/village-piople/js/bootstrap.min.js"></script>
+  </body>
+</html>
